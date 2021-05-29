e4fd08e5d022752cf40ac6e117a0dd982556757c
Luis Lopez <darkdestruktion@gmail.com>
2021-05-29 15:28:37 -0500

    needs to order the routes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,12 @@
 import React, { Component }from 'react';
 import './App.css';
-import Navigation from '../components/Navigation';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'; 
+import { LinkContainer } from 'react-router-bootstrap'; // It wraps the nav.link tags to keep the styling and keeping the funcionality of the Links from react-router-dom
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Show from '../components/show/Show';
+
 
 class App extends Component {
   constructor(){
@@ -15,7 +21,40 @@ class App extends Component {
         <div className="App">
           <h1 className="title">Welcome to simple crud</h1>
         </div>
-        <Navigation/>
+        <Navbar bg="dark" variant="dark">
+              <Container>
+                <Navbar.Brand>Simple Crud</Navbar.Brand>
+                  <BrowserRouter>
+                      <Nav className="me-auto">
+                          <LinkContainer to="/insert">
+                              <Nav.Link>Insert data</Nav.Link>
+                          </LinkContainer>
+                          <LinkContainer to="/edit">
+                              <Nav.Link>Edit data</Nav.Link>
+                          </LinkContainer>
+                          <LinkContainer to="/show">
+                              <Nav.Link>Show data</Nav.Link>
+                          </LinkContainer>
+                          <LinkContainer to="/delete">
+                              <Nav.Link>Delete data</Nav.Link>
+                          </LinkContainer>
+                      </Nav>
+                          <Switch>
+                              <Route path="/insert">
+                                      
+                              </Route>
+                              <Route path="/edit">
+                                  
+                              </Route>
+                              <Route path="/show" component={Show}>
+                            </Route>
+                              <Route path="/delete">
+                                  
+                              </Route>
+                          </Switch>
+                  </BrowserRouter>
+              </Container>
+            </Navbar>
       </div>
     );
   }
